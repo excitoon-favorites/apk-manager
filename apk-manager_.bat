cb0a6fe4dfdfa4d2c133e5fb9497ba6aab9abbe1
JordanBancino <factshackstips@gmail.com>
2017-04-21 16:43:35 -0400

    Script Explained

--- apk-manager_.bat
+++ apk-manager_.bat
@@ -1,3 +1,6 @@
+:: This is installed to %systemdrive%\Windows
+:: it executes apk-manager from it's path. Only Used if installed to system
+:: WARNING! This feature may be removed completely
 @echo off
 echo Loading...
 set runin=%cd%
