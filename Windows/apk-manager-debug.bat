005e9b074f0f7eb0cb79adabe208db00ca1c9aa3
JordanBancino <factshackstips@gmail.com>
2017-05-27 15:28:35 -0400

    Fixed. This component actually works now

--- Windows/apk-manager-debug.bat
+++ Windows/apk-manager-debug.bat
@@ -1,9 +1,15 @@
 @echo off
-:: we need to run a verify of our own
+if not exist "apk-manager.bat" (
+    if not exist "apk-manager.exe" (
+        goto Fail
+    )
+    goto Fail
+)
 
-:: invoke apk-manager, but in debug mode, otherwise known as "log all output to a text file"
-:: This only works if you have windows 7 or higher, because of PowerShell
-powershell ".\apk-manager %1 %2 | tee -Append apk-manager_debug.txt"
+apk-manager.bat %1 %2 %3 | .\Bin\tee.exe apk-manager_debug.txt
 echo. >> apk-manager_debug.txt
-:: For some reason, all output by the JAVA Programs used in this project output Asian characters.
-:: we are working on this
+goto :eof
+
+:Fail
+echo Unable to proceed. Components Missing, or improperly placed.
+goto :eof
