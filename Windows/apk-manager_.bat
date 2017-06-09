b20b846cb57a843d330975335171ff227cd71e33
Jordan Bancino <factshackstips@gmail.com>
2017-06-09 15:42:21 -0400

    Remove System Install Method
    It's horribly buggy, and currently provides no purpose.

--- Windows/apk-manager_.bat
+++
@@ -1,9 +0,0 @@
-:: This is installed to %systemdrive%\Windows
-:: it executes apk-manager from it's path. Only Used if installed to system
-:: WARNING! This feature may be removed completely
-@echo off
-echo Loading...
-set runin=%cd%
-cd "%userprofile%\apk-manager\"
-apk-manager.bat %1 %2 %3
-cd %runin%
